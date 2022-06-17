c95fc85f02b4426e11d5b0e69a587443b760a66f
jonathan-pasco-arnone <60712726+jonathan-pasco-arnone@users.noreply.github.com>
2022-06-17 11:07:10 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,25 @@
+function gobackword () {
+    robotbit.MotorRunDual(
+    robotbit.Motors.M2A,
+    leftDCDefault,
+    robotbit.Motors.M1A,
+    rightDCDefault
+    )
+}
+function goforword () {
+    robotbit.MotorRunDual(
+    robotbit.Motors.M2A,
+    leftDCDefault,
+    robotbit.Motors.M1B,
+    rightDCDefault
+    )
+}
+let rightDCDefault = 0
+let leftDCDefault = 0
 let penDown = 180
 let penUp = 120
-let leftDCDefault = -200
-let rightDCDefault = 100
-basic.forever(function () {
-    robotbit.MotorRunDual(
-    robotbit.Motors.M1A,
-    -255,
-    robotbit.Motors.M2A,
-    leftDCDefault
-    )
-    basic.pause(1000)
-    leftDCDefault = 200
-    robotbit.MotorRunDual(
-    robotbit.Motors.M1A,
-    -255,
-    robotbit.Motors.M2A,
-    leftDCDefault
-    )
-    basic.pause(1000)
+leftDCDefault = -200
+rightDCDefault = 200
+loops.everyInterval(1000, function () {
+    goforword()
 })
