50c84a8542e3f39210d29358551e2972667ba5b5
jonathan-pasco-arnone <60712726+jonathan-pasco-arnone@users.noreply.github.com>
2022-06-20 10:04:41 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,49 @@
-function gobackword () {
-    robotbit.MotorRunDual(
-    robotbit.Motors.M2A,
-    leftDCDefault,
-    robotbit.Motors.M1A,
-    rightDCDefault
-    )
+function turnLeft (num: number) {
+    for (let index = 0; index < num; index++) {
+        robotbit.MotorRunDual(
+        robotbit.Motors.M2A,
+        leftDCDefault * -1,
+        robotbit.Motors.M1B,
+        rightDCDefault
+        )
+    }
 }
-function goforword () {
-    robotbit.MotorRunDual(
-    robotbit.Motors.M2A,
-    leftDCDefault,
-    robotbit.Motors.M1B,
-    rightDCDefault
-    )
+function goBackwords (num: number) {
+    for (let index = 0; index < num; index++) {
+        robotbit.MotorRunDual(
+        robotbit.Motors.M2A,
+        leftDCDefault * -1,
+        robotbit.Motors.M1A,
+        rightDCDefault * -1
+        )
+    }
+}
+function goForwards (num: number) {
+    for (let index = 0; index < num; index++) {
+        robotbit.MotorRunDual(
+        robotbit.Motors.M2A,
+        leftDCDefault,
+        robotbit.Motors.M1B,
+        rightDCDefault
+        )
+    }
+}
+function turnRight (num: number) {
+    for (let index = 0; index < num; index++) {
+        robotbit.MotorRunDual(
+        robotbit.Motors.M2A,
+        leftDCDefault,
+        robotbit.Motors.M1B,
+        rightDCDefault * -1
+        )
+    }
 }
 let rightDCDefault = 0
 let leftDCDefault = 0
 let penDown = 180
 let penUp = 120
-leftDCDefault = -200
-rightDCDefault = 200
-loops.everyInterval(1000, function () {
-    goforword()
+leftDCDefault = -100
+rightDCDefault = 94
+basic.forever(function () {
+    turnLeft(1)
 })
