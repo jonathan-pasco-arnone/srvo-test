d40c5fca7f2821c71e94fe218b1d7c074f43ef3c
jonathan-pasco-arnone <60712726+jonathan-pasco-arnone@users.noreply.github.com>
2022-06-22 09:31:58 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,22 @@
-function turnLeft (num: number) {
-    for (let index = 0; index < num; index++) {
+function turnLeft (degrees: number) {
+    for (let index = 0; index < degrees * 3; index++) {
         robotbit.MotorRunDual(
         robotbit.Motors.M2A,
-        leftDCDefault * -1,
+        0,
         robotbit.Motors.M1B,
-        rightDCDefault
+        rightDCDefault + 30
         )
     }
+    robotbit.MotorRunDual(
+    robotbit.Motors.M2A,
+    0,
+    robotbit.Motors.M1B,
+    0
+    )
+    basic.pause(100)
 }
 function goBackwords (num: number) {
-    for (let index = 0; index < num; index++) {
+    for (let index = 0; index < num * 100; index++) {
         robotbit.MotorRunDual(
         robotbit.Motors.M2A,
         leftDCDefault * -1,
@@ -17,9 +24,20 @@ function goBackwords (num: number) {
         rightDCDefault * -1
         )
     }
+    robotbit.MotorRunDual(
+    robotbit.Motors.M2A,
+    0,
+    robotbit.Motors.M1B,
+    0
+    )
+    basic.pause(100)
+}
+function SetPenUp () {
+    robotbit.Servo(robotbit.Servos.S1, penUp)
+    basic.pause(100)
 }
 function goForwards (num: number) {
-    for (let index = 0; index < num; index++) {
+    for (let index = 0; index < num * 100; index++) {
         robotbit.MotorRunDual(
         robotbit.Motors.M2A,
         leftDCDefault,
@@ -27,23 +45,49 @@ function goForwards (num: number) {
         rightDCDefault
         )
     }
+    robotbit.MotorRunDual(
+    robotbit.Motors.M2A,
+    0,
+    robotbit.Motors.M1B,
+    0
+    )
+    basic.pause(100)
 }
-function turnRight (num: number) {
-    for (let index = 0; index < num; index++) {
+function turnRight (degrees: number) {
+    for (let index = 0; index < degrees * 2; index++) {
         robotbit.MotorRunDual(
         robotbit.Motors.M2A,
-        leftDCDefault,
+        leftDCDefault + 55,
         robotbit.Motors.M1B,
-        rightDCDefault * -1
+        0
         )
     }
+    robotbit.MotorRunDual(
+    robotbit.Motors.M2A,
+    0,
+    robotbit.Motors.M1B,
+    0
+    )
+    basic.pause(100)
+}
+function setPenDown () {
+    robotbit.Servo(robotbit.Servos.S1, penDown)
+    basic.pause(100)
 }
 let rightDCDefault = 0
 let leftDCDefault = 0
-let penDown = 180
-let penUp = 120
-leftDCDefault = -100
-rightDCDefault = 94
-basic.forever(function () {
-    turnLeft(1)
-})
+let penUp = 0
+let penDown = 0
+penDown = 160
+penUp = 130
+leftDCDefault = 190
+rightDCDefault = 130
+SetPenUp()
+basic.pause(2000)
+goForwards(5)
+turnRight(90)
+goForwards(5)
+turnRight(90)
+goForwards(5)
+turnRight(90)
+goForwards(5)
